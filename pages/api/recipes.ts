068ad955d8a59f3d4ecaa525f3662eca70d71487
Maol-commits <martinalejandro1997@gmail.com>
2023-02-21 00:38:57 -0300

    add server-side image validation ✅

--- pages/api/recipes.ts
+++ pages/api/recipes.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import cloudinary from 'cloudinary'
@@ -27,17 +28,31 @@ async function onGet (req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(recipes)
 }
 
-async function onPost (req: NextApiRequest, res: NextApiResponse) { // TODO: validate image before uploading to cloudinary
+async function onPost (req: NextApiRequest, res: NextApiResponse) { // TODO: validate image before uploading to cloudinary ✅
+  const magic = {
+    jpg: 'ffd8ffe0',
+    png: '89504e47',
+    gif: '47494638'
+  }
   let image
   const { title, description, prepTime, complexity, steps, ingredients, img } = req.body
   if (img && img.length > 0 && img instanceof Array) {
     image = await Promise.all(
       img.map(async (i) => {
+        const base64ToHex = (i: string) => Buffer.from(i.split('base64,')[1], 'base64').toString('hex')
+        const first4Bytes = base64ToHex(i).slice(0, 8)
+        if (first4Bytes !== magic.jpg && first4Bytes !== magic.png && first4Bytes !== magic.gif) {
+          return
+        }
         return await cloudinary.v2.uploader.upload(i)
       })
     )
   } else {
     res.status(400).json({ message: 'Image is required' })
+    return
+  }
+  if (image.some((i) => !i || !i.secure_url)) {
+    res.status(400).json({ message: 'File is not an image' })
     return
   }
   const ingre = ingredients.replace(' y ', ',').split(',').map((i: string) => i.trim())
